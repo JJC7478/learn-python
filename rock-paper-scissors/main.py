5bafec9c49a5f5b5e5467ef7a47b2c002b0835a0
Jbangsensei <johnjonahc@gmail.com>
2022-06-17 23:29:34 -0700

    Finished ai and scoreboard functionality

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,25 +1,7 @@
 from random import *
+from ui import UI
 
-MOVES = ["rock", "paper", "scissors"]
 
-ai_move = choice(MOVES)
+ui = UI()
 
-player_move = input("Rock, paper, or scissors?").lower()
-
-if ai_move == player_move:
-    print("draw")
-elif player_move == "rock":
-    if ai_move == "paper":
-        print("ai wins")
-    elif ai_move == "scissors":
-        print("player wins")
-elif player_move == "paper":
-    if ai_move == "rock":
-        print("player wins")
-    elif ai_move == "scissors":
-        print("ai wins")
-elif player_move == "scissors":
-    if ai_move == "rock":
-        print("ai wins")
-    elif ai_move == "paper":
-        print("player wins")
+#Next time, do not shoehorn OOP
--- rock-paper-scissors/ui.py
+++ rock-paper-scissors/ui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import random as r
 
 WINDOW_COLOR = "#DFF6FF"
 CANVAS_HEIGHT = 300
@@ -14,6 +15,8 @@ LABEL_FONT = ("Impact", 25, "bold")
 ROCK_PNG = "images/rock.png"
 PAPER_PNG = "images/paper.png"
 SCISSORS_PNG = "images/scissors.png"
+MOVES = ["rock", "paper", "scissors"]
+
 
 class UI():
     def __init__(self) -> None:
@@ -23,6 +26,10 @@ class UI():
         self.window.title("Rock Paper Scissors")
         self.window.config(padx=20, pady=20, bg=WINDOW_COLOR)
 
+        #Moves
+        self.player_choice = ""
+        self.ai_choice = ""
+
         #Score
         self.player_score = 0
         self.ai_score = 0
@@ -31,6 +38,12 @@ class UI():
         self.rock_img = self.convert_img(ROCK_PNG)
         self.paper_img = self.convert_img(PAPER_PNG)
         self.scissors_img = self.convert_img(SCISSORS_PNG)
+
+        self.img_dict = {
+            "rock": self.rock_img,
+            "paper": self.paper_img,
+            "scissors": self.scissors_img
+        }
         
         #Canvas
         self.player_canvas = tk.Canvas(bg=PLAYER_COLOR, height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
@@ -42,7 +55,7 @@ class UI():
         self.ai_canvas.grid(column=3, row=1, pady=CANVAS_PADDING)
 
         self.score_canvas = tk.Canvas(bg=WINDOW_COLOR, height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
-        self.score_canvas.create_text(
+        self.score = self.score_canvas.create_text(
             150, 
             150, 
             text=f"{self.player_score}:{self.ai_score}", 
@@ -112,12 +125,44 @@ class UI():
 
     def choose_rock(self):
         self.player_canvas.itemconfig(self.player_img, image=self.rock_img)
-        return "rock"
+        self.player_choice = "rock"
+        self.ai_choose()
+        self.update_score(self.player_choice, self.ai_choice)
 
     def choose_paper(self):
         self.player_canvas.itemconfig(self.player_img, image=self.paper_img)
-        return "paper"
+        self.player_choice = "paper"
+        self.ai_choose()
+        self.update_score(self.player_choice, self.ai_choice)
 
     def choose_scissors(self):
         self.player_canvas.itemconfig(self.player_img, image=self.scissors_img)
-        return "scissors"
+        self.player_choice = "scissors"
+        self.ai_choose()
+        self.update_score(self.player_choice, self.ai_choice)
+    
+    def ai_choose(self):
+        self.ai_choice = r.choice(MOVES)
+        self.ai_canvas.itemconfig(self.ai_img, image=self.img_dict[self.ai_choice])
+    
+    def update_score(self, player_choice, ai_choice):
+        if ai_choice == player_choice:
+            pass
+        elif player_choice == "rock":
+            if ai_choice == "paper":
+                self.ai_score += 1
+            elif ai_choice == "scissors":
+                self.player_score += 1
+        elif player_choice == "paper":
+            if ai_choice == "rock":
+                self.player_score += 1
+            elif ai_choice == "scissors":
+                self.player_score += 1
+        elif player_choice == "scissors":
+            if ai_choice == "rock":
+                self.player_score += 1
+            elif ai_choice == "paper":
+                self.player_score += 1
+        self.score_canvas.itemconfig(self.score, text=f"{self.player_score}:{self.ai_score}")
+    
+    
