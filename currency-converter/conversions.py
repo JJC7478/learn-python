b5f650c87edfc4b8e2a6334ffe30703f5b3f562f
Jbangsensei <johnjonahc@gmail.com>
2022-06-21 19:15:01 -0700

    Added conversion ratios

---
+++ currency-converter/conversions.py
@@ -0,0 +1,32 @@
+conversions = {
+    "US Dollar": {
+        "Euro": 0.95,
+        "Japanese Yen": 135,
+        "Korean Won": 1292,
+        "Canadian Dollar": 1.3
+    },
+    "Euro": {
+        "US Dollar": 1.05,
+        "Japanese Yen": 142,
+        "Korean Won": 1361,
+        "Canadian Dollar": 1.37
+    },
+    "Japanese Yen": {
+        "US Dollar": 0.0073,
+        "Euro": 0.007,
+        "Korean Won": 9.51,
+        "Canadian Dollar": 0.0095
+    },
+    "Korean Won": {
+        "US Dollar": 0.0008,
+        "Euro": 0.0007,
+        "Japanese Yen": 0.105,
+        "Canadian Dollar": 0.001
+    },
+    "Canadian Dollar": {
+        "US Dollar": 0.772,
+        "Euro": 0.734,
+        "Japanese Yen": 105,
+        "Korean Won": 1000
+    }
+}
